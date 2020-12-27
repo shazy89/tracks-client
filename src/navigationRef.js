a35770be1e0caabcf34a24024f1db401321c25ab
shazy89 <erdo.shazy123@gmail.com>
2020-12-27 17:11:13 -0500

    create Auth Form and wire up with SignupScreen

--- src/navigationRef.js
+++ src/navigationRef.js
@@ -1,6 +1,7 @@
 import { NavigationActions } from 'react-navigation'
 let navigator;
-
+// this is the way we can handle navigation outside of the APP
+// I want to navigate from AuthContext and it is outside of react navigator so vi create custom path
 export const setNavigator = (nav) => {
   navigator = nav
 };
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,55 +1,47 @@
-import React, { useState, useContext } from 'react'
-import { View, StyleSheet} from 'react-native';
-import { Text, Input, Button } from 'react-native-elements'
-import Spacer from '../components/Spacer'
-import { Context as AuthContext } from '../context/AuthContext'
-const SignupScreen = ({ navigation }) => {
-    const { state, signup } = useContext(AuthContext);
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-      
+import React, { useContext } from 'react';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text } from 'react-native-elements';
+import Spacer from '../components/Spacer';
+import { Context as AuthContext } from '../context/AuthContext';
+import AuthForm from '../components/AuthForm';
 
-    return <View style={styles.container}>
-     <Spacer>
-        <Text h2 > Sign Up for 
-           <Text h1 style={{color: 'red'}}> Tracker  </Text>
-        </Text>
-      </Spacer>
-      <Spacer />
-        <Input label="Email" 
-                value={email} onChangeText={setEmail} 
-                autoCapitalize="none" autoCorrect={false} />
-      <Spacer />
-        <Input label="Password" 
-               value={password} onChangeText={setPassword}  
-               autoCapitalize="none" autoCorrect={false} secureTextEntry/>
-               {state.errorMessage ? <Text style={styles.errMessage}> {state.errorMessage}</Text> : null}
-      <Spacer>
-        <Button title="Sign Up" onPress={() => signup({ email, password })} />
-      </Spacer>
+const SignupScreen = ({ navigation }) => {
+  const { state, signup } = useContext(AuthContext);
+
+  return (
+    <View style={styles.container}>
+      <AuthForm
+        headerText="Sign Up for Tracker"
+        errorMessage={state.errorMessage}
+        submitButtonText="Sign Up"
+        onSubmit={signup}
+      />
+      <TouchableOpacity onPress={() => navigation.navigate('Signin')}>
+        <Spacer>
+          <Text style={styles.link}>
+            Already have an account? Sign in instead
+          </Text>
+        </Spacer>
+      </TouchableOpacity>
     </View>
+  );
 };
+
 SignupScreen.navigationOptions = () => {
-    return {
-        headerShown: false
-    }
-}
+  return {
+    header: () => false,
+  };
+};
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      marginBottom: 250
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 250,
   },
-  errMessage: {
-    color: 'red',
-    fontSize: 16,
-    marginLeft: 15
-    
-
-  }
+  link: {
+    color: 'blue',
+  },
 });
 
-
-
-export default SignupScreen  
+export default SignupScreen;
