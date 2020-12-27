b64bc00a14580f6a1f7c7d919830415db17e293d
shazy89 <erdo.shazy123@gmail.com>
2020-12-27 12:27:12 -0500

    signup screen wired up using Context, Users can create new account and we have axxex to the JWT token

---
+++ src/api/trackerApi.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+   baseURL: 'http://4b4ce07087e9.ngrok.io'
+});
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,5 @@
 import createDataContext from './createDataContext';
+import trackerApi from '../api/trackerapi';
 
 const authReducer = (state, action) => {
     switch (action.type) {
@@ -8,8 +9,13 @@ const authReducer = (state, action) => {
 };
 
 const signup = (dispatch) => {
-    return ({email, password}) => {
-
+    return async ({email, password}) => {
+      try {
+         const response = await trackerApi.post('/signup', { email, password });
+         console.log(response.data)
+      } catch (err) {
+        console.log(err.response.data)
+      }
     };
 };
 
