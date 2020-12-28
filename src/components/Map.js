df983bf5f5f2eabc3f8b308bf982a12462c946ee
shazy89 <erdo.shazy123@gmail.com>
2020-12-28 17:12:13 -0500

    display map on Track Create Screen

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import { View, StyleSheet, Text} from 'react-native';
+import MapView from 'react-native-maps'
 
 const Map = () => {
-    return <Text style ={{fontSize: 48}}> I'm a Map  </Text>
+    return <MapView 
+    initialRegion={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+    style={styles.map}/>
 };
 const styles = StyleSheet.create({
-
+ map: {
+     height: 300
+ }
 });
 
 export default Map;
