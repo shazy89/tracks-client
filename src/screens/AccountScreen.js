81ca5b3de3906ee22b59baac56872f9a11cd0978
shazy89 <erdo.shazy123@gmail.com>
2020-12-28 16:53:15 -0500

    handle SignOut screen add Safe Area View

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
 import { View, StyleSheet} from 'react-native';
+import { SafeAreaView } from 'react-navigation'
 import { Text, Button, Input } from 'react-native-elements';
 import Spacer from '../components/Spacer';
 import { Context as AuthContext } from '../context/AuthContext';
@@ -7,10 +8,13 @@ import { Context as AuthContext } from '../context/AuthContext';
 const AccountScreen = () => {
     const { signout } = useContext(AuthContext);
     return <>
-            <Spacer>
-               <Button title="Sign Out" onPress={signout}></Button>
-            </Spacer>
-          </>
+            <SafeAreaView forceInset={{top: 'always'}}>
+               <Spacer>
+                   <Text style={{fontSize: 45}}>AccountScreen</Text>
+                  <Button title="Sign Out" onPress={signout}></Button>
+               </Spacer>
+            </SafeAreaView>
+         </>
 };
 const styles = StyleSheet.create({
 
