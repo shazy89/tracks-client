0a198da9891ca15ed6225797105d5dd239a2a430
shazy89 <erdo.shazy123@gmail.com>
2020-12-28 16:58:37 -0500

    map component

---
+++ src/components/Map.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, StyleSheet, Text} from 'react-native';
+
+const Map = () => {
+    return <Text style ={{fontSize: 48}}> I'm a Map  </Text>
+};
+const styles = StyleSheet.create({
+
+});
+
+export default Map;
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,8 +1,16 @@
-import React from 'react'
-import { View, StyleSheet, Text} from 'react-native';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Button, Input, Text } from 'react-native-elements';
+import { SafeAreaView } from 'react-navigation';
+import Map from '../components/Map';
 
 const TrackCreateScreen = () => {
-    return <Text style ={{fontSize: 48}}> TrackCreateScreen  </Text>
+    return <>
+            <SafeAreaView forceInset={{top: 'always'}}>
+               <Text h1> TrackCreateScreen  </Text>
+               <Map />
+            </SafeAreaView>
+          </>
 };
 const styles = StyleSheet.create({
 
