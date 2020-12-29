e933a39060e6b1fa5564a1b1743c9f213ff6cbae
shazy89 <erdo.shazy123@gmail.com>
2020-12-29 14:21:10 -0500

    startRecording, stopRecording functions , add code to reducer

--- src/context/LocationContext.js
+++ src/context/LocationContext.js
@@ -4,15 +4,28 @@ const locationReducer = (state, action) => {
   switch (action.type) {
     case 'add_current_location':
       return { ...state, currentLocation: action.payload };
+    case 'start_recording':
+      return {...state, recording: true };
+    case 'stop_recording':
+      return {...state, recording: false};
+    case 'add_location': 
+      return { ...state, location: [...state.locations, action.payload] }     ; 
     default:
       return state;
   }
 };
 
-const startRecording = dispatch => () => {};
-const stopRecording = dispatch => () => {};
-const addLocation = dispatch => location => {
+const startRecording = dispatch => () => {
+  dispatch({type: 'start_recording'})
+};
+const stopRecording = dispatch => () => {
+  dispatch({type: 'stop_recording'})
+};
+const addLocation = dispatch => (location, recording) => {
     dispatch({ type: 'add_current_location', payload: location });
+    if (recording) {
+      dispatch({type: 'add_location', payload: location})
+    }
   };
 
 
