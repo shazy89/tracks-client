9c416198f04a00dc61f97f53d5aa5dc030e6dd9b
shazy89 <erdo.shazy123@gmail.com>
2020-12-29 16:26:14 -0500

    save button, create new TrackContext

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,7 +5,7 @@ import { Context as LocationContext } from '../context/LocationContext';
 
 const Map = () => {
   const {
-    state: { currentLocation }
+    state: { currentLocation, locations }
   } = useContext(LocationContext);
 
   if (!currentLocation) {
@@ -30,6 +30,7 @@ const Map = () => {
                radius={30}
                strokeColor="rgba(158, 158, 255, 1.0)"
                fillColor="rgba(158, 158, 255, 0.3)"/>
+        <Polyline coordinates={locations.map(loc => loc.coords)} />       
     </MapView>
   );
 };
--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { Input, Button } from 'react-native-elements';
 import Spacer from './Spacer';
 import { Context as LocationContext } from '../context/LocationContext';
+import { LocationSubscriber } from 'expo-location/build/LocationSubscribers';
 
 const TrackForm = () => {
   const {
@@ -22,11 +23,20 @@ const TrackForm = () => {
           placeholder="Enter name"
         />
       </Spacer>
+      <Spacer>
       {recording ? (
         <Button title="Stop" onPress={stopRecording} />
       ) : (
         <Button title="Start Recording" onPress={startRecording} />
       )}
+      </Spacer>
+       <Spacer>
+      {
+          !recording && locations.length 
+          ? (<Button title= "Save Recording" />)
+          : null
+      }
+      </Spacer>
     </>
   );
 };
