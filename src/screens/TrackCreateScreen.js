19d7748ff64be61d4b94c4dac07afaa4038cbe8c
shazy89 <erdo.shazy123@gmail.com>
2020-12-29 11:07:02 -0500

    addLocation function, store data inside the reducer

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,12 +1,13 @@
 import '../_mockLocation'
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { StyleSheet } from 'react-native';
 import { Button, Input, Text } from 'react-native-elements';
 import { SafeAreaView } from 'react-navigation';
 import { requestPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location'
 import Map from '../components/Map';
-
+import { Context as LocationContext} from '../context/LocationContext'
 const TrackCreateScreen = () => {
+    const { addLocation } = useContext(LocationContext)
    const [err, setErr] = useState(null);
 
    const startWatching = async () => {
@@ -17,7 +18,7 @@ const TrackCreateScreen = () => {
          timeInterval: 1000,
          distanceInterval: 10 
       }, location => {
-         // console.log(location)
+         addLocation(location)
       });
       if (!granted) {
         throw new Error('Location permission not granted');
