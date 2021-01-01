bfccc942f80e89992e8efd81b14aab9c30f3f4a1
shazy89 <erdo.shazy123@gmail.com>
2021-01-01 07:41:39 -0500

    track list screen

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { View, StyleSheet, Text, Button} from 'react-native';
+import { NavigationEvents } from 'react-navigation';
 
 const TrackListScreen = ({navigation}) => {
     return <>
+             <NavigationEvents onWillFocus={() => console.log('hey there')}/>
             <Text style ={{fontSize: 48}}> TrackListScreen </Text>
             <Button title="Go to Track Detail" onPress={() => navigation.navigate("TrackDetail") }/>
            </>
