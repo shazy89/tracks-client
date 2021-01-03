5a334dad62f3ef253f431b59cb2267236ce2c56e
shazy89 <erdo.shazy123@gmail.com>
2021-01-03 12:35:35 -0500

    Import map View from react-native-maps, track find function , use MapView inside the TrackDetailScreen

--- src/screens/TrackDetailScreen.js
+++ src/screens/TrackDetailScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { View, StyleSheet, Text} from 'react-native';
 import { Context as TrackContext} from '../context/TrackContext';
+import MapView, { PolyLine} from 'react-native-maps';
 
 const TrackDetailScreen = ( { navigation  } ) => {
     const { state } = useContext(TrackContext);
@@ -8,7 +9,15 @@ const TrackDetailScreen = ( { navigation  } ) => {
 
     const track = state.find(t => t._id === _id)
 
-    return <Text style ={{fontSize: 48}}> {track.name}  </Text>
+    return  <>
+              <Text style ={{fontSize: 48}}> {track.name}  </Text>
+              <MapView initialRegion={{
+                  longitudeDelta: 0.01,
+                  latitudeDelta: 0.01,
+              }}> 
+                  <PolyLine coordinates={track.locations.map(loc => loc.coords)} />
+              </MapView>
+            </>
 };
 const styles = StyleSheet.create({
 
