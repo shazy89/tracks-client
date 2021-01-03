060c13d567a7dfd3b34b7c8b67d74ab037a1a5ac
shazy89 <erdo.shazy123@gmail.com>
2021-01-03 12:28:33 -0500

    wiredup trackDetaulScreen with TeackListScreen

--- src/screens/TrackDetailScreen.js
+++ src/screens/TrackDetailScreen.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react';
 import { View, StyleSheet, Text} from 'react-native';
+import { Context as TrackContext} from '../context/TrackContext';
 
 const TrackDetailScreen = ( { navigation  } ) => {
-    const _id = navigation.getParam('_id')
-    
-    return <Text style ={{fontSize: 48}}> TrackDetailScreen  </Text>
+    const { state } = useContext(TrackContext);
+    const _id = navigation.getParam('_id');
+
+    const track = state.find(t => t._id === _id)
+
+    return <Text style ={{fontSize: 48}}> {track.name}  </Text>
 };
 const styles = StyleSheet.create({
 
